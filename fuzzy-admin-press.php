33a339dd2214f55a1ee4cc35cd35ab9051b7dc1e
Ollie Jones <olliejones@gmail.com>
2025-03-26 14:38:35 -0400

    #2 User preference to turn searching on/off

--- fuzzy-admin-press.php
+++ fuzzy-admin-press.php
@@ -18,26 +18,71 @@
 namespace FuzzyAdminPress;
 
 \add_action( 'admin_init', '\FuzzyAdminPress\admin_init', 10, 0 );
+\add_action( 'personal_options', '\FuzzyAdminPress\personal_options' ,10, 1 );
+\add_action( 'personal_options_update', '\FuzzyAdminPress\save_personal_options' );
+\add_action( 'edit_user_profile_update', '\FuzzyAdminPress\save_personal_options' );
+
 
 function admin_init() {
-	$version = '0.1.0';
+  $version = '0.1.0';
 
-	load_plugin_textdomain( 'fuzzy-admin-press', false, dirname( plugin_basename( __FILE__ ) ) . '/languages/' );
+  load_plugin_textdomain( 'fuzzy-admin-press', false, dirname( plugin_basename( __FILE__ ) ) . '/languages/' );
 
-	wp_enqueue_style( 'jquery-ui-autocomplete' );
-	wp_enqueue_style( 'fuzzy-admin-press', plugin_dir_url( __FILE__ ) . 'assets/css/fuzzy.css', array(), $version, 'all' );
-	wp_enqueue_script( 'fuzzy-admin-press', plugin_dir_url( __FILE__ ) . 'assets/js/fuzzy.js', array( 'jquery-ui-autocomplete' ), $version, true );
+  if ( get_searchable_menu_pref()) {
+    wp_enqueue_style( 'jquery-ui-autocomplete' );
+    wp_enqueue_style( 'fuzzy-admin-press', plugin_dir_url( __FILE__ ) . 'assets/css/fuzzy.css', array(), $version, 'all' );
+    wp_enqueue_script( 'fuzzy-admin-press', plugin_dir_url( __FILE__ ) . 'assets/js/fuzzy.js', array( 'jquery-ui-autocomplete' ), $version, true );
 
-	$i18n = array(
-		/* translators: name of plugin to appear as the placeholder in the search box. */
-		'placeholder'        => __( 'Shift Shift Search', 'fuzzy-admin-press' ),
-		// Intentional use of core localization strings.
-        // phpcs:ignore WordPress.WP.I18n.MissingArgDomain
-		'placeholder_active' => implode( ' ', array( __( 'Search' ), __( 'Menus' ) ) ),
-		/* translators: this is the delimiter between menu and submenu. For example Settings > Geheral. Change for RTL languages  to  ⮜*/
-		'submenu_delimiter' => __(' ⮞ ','fuzzy-admin-press'),
-		'locale'             => get_user_locale(),
-	);
-	wp_localize_script( 'fuzzy-admin-press', 'fuzzy_admin_press_i18n', $i18n );
+    $i18n = array(
+      /* translators: name of plugin to appear as the placeholder in the search box. */
+      'placeholder'        => __( 'Shift Shift Search', 'fuzzy-admin-press' ),
+      // Intentional use of core localization strings.
+      // phpcs:ignore WordPress.WP.I18n.MissingArgDomain
+      'placeholder_active' => implode( ' ', array( __( 'Search' ), __( 'Menus' ) ) ),
+      /* translators: this is the delimiter between menu and submenu. For example Settings > Geheral. Change for RTL languages  to  ⮜*/
+      'submenu_delimiter'  => __( ' ⮞ ', 'fuzzy-admin-press' ),
+      'locale'             => get_user_locale(),
+    );
+    wp_localize_script( 'fuzzy-admin-press', 'fuzzy_admin_press_i18n', $i18n );
+  }
+}
+
+function get_searchable_menu_pref( $user = 0 ) {
+  $pref = get_user_option( 'searchable_admin_menu', $user );
+  if ( false === $pref ) {
+    return true;
+  }
+
+  return 'true' === $pref;
+}
+
+function personal_options( $profile_user ) {
+  ?>
+  <tr class="show-admin-bar user-admin-bar-front-wrap">
+    <th scope="row"><?php _e( 'Searchable Menus', 'fuzzy-admin-press' ); ?></th>
+    <td>
+      <label for="searchable_menus">
+        <input name="searchable_menus" type="checkbox" id="searchable_menus"
+               value="1"<?php checked( get_searchable_menu_pref( $profile_user->ID ) ); ?> />
+        <?php _e( 'Searchable aministration menus', 'fuzzy-admin-press' ); ?>
+      </label><br/>
+    </td>
+  </tr>
+  <?php
+}
+
+
+function save_personal_options( $user_id ) {
+  if ( empty( $_POST['_wpnonce'] ) || ! wp_verify_nonce( $_POST['_wpnonce'], 'update-user_' . $user_id ) ) {
+    return;
+  }
+
+  if ( ! current_user_can( 'edit_user', $user_id ) ) {
+    return;
+  }
+  $menu = $_POST['searchable_menus'];
+  $menu = '1' === $menu || 'on' === $menu ? 'true' : 'false';
+
+  update_user_meta( $user_id, 'searchable_admin_menu', $menu );
 }
 
